d888cd4ed102e9a54378ab95c622885d83a7c024
Peter Burgess <petebur@gmail.com>
2015-04-26 18:56:11 -0700

    Initial Commit

---
+++ Tic Tac Toe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tic Tac Toe
+//
+//  Created by Peter Burgess on 2015-04-26.
+//  Copyright (c) 2015 Peter Burgess. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
